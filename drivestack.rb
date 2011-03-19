38ec57120d10c77cec6da14092ee74a252e7d497
Joseph Chysler <jdchrysl@svsu.edu>
2011-03-19 14:05:03 -0400

    not sure

--- drivestack.rb
+++ drivestack.rb
@@ -9,13 +9,13 @@ require 'dm-types'
 require 'ccsv'
 
 configure :development do
-    DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/_drives.db")
+    DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/_drives2.db")
 end
 
 require 'models/drive'
 
 configure :development do
-  DataMapper.auto_upgrade!
+  DataMapper.auto_migrate!
 end
 
 # set utf-8 for outgoing
@@ -33,23 +33,38 @@ get '/parse' do
 
   Ccsv.foreach('data.csv') do |row|
     counter += 1
-    newdrive = {:bn => row[0],
-      :mn           => row[1],
-      :brand        => row[2],
-      :size         => row[3],
-      :speed        => row[4],
-      :interface    => row[5]}
-    @drive = Drive.new(newdrive)
-    if @drive.save
-      line = "#{row[0]},#{row[1]},#{row[2]},#{row[3]},#{row[4]},#{row[5]}"
-      result += "drive #{counter} saved as ##{@drive.id}<br />\n"
-      File.open('done.csv', 'a') { |f| f.puts line }
-    else
-      result = "failure"
+    
+    @models = DriveModel.get(:model_number => row[1])
+    if (@models == nil)
+      newModel = {:model_number => row[1],
+                  :brand        => row[2],
+                  :size         => row[3],
+                  :speed        => row[4],
+                  :interface    => row[5]}
+      @model = DriveModel.new(newModel)
+      @model.save
     end
+    newdrive = {:bn => row[0]}
+    @drive = Drive.new(newdrive)
+    unless (@drive.save)
+      result = :failure
+    end
+      
+
+
+    File.open('models.txt', 'a') { |f| f.puts @models }
+
+    #@drive = Drive.new(newdrive)
+    #if @drive.save
+      #line = "#{row[0]},#{row[1]},#{row[2]},#{row[3]},#{row[4]},#{row[5]}"
+      #result += "drive #{counter} saved as ##{@drive.bn}<br />\n"
+      #File.open('done.csv', 'a') { |f| f.puts line }
+    #else
+      #result = "failure"
+    #end
   end
 
-  if result == "failure"
+  if result == :failure
     result
   else
     redirect('/list')
@@ -58,11 +73,11 @@ end
 
 get '/list' do
   @title = 'List Drives'
-  @drives = Drive.all(:order => [:interface.asc, :speed.asc, :size.asc])
+  @drives = Drive.all()
   haml :list
 end
-get '/forget/:id' do
-  drive = Drive.get(params[:id])
+get '/forget/:bn' do
+  drive = Drive.get(params[:bn])
   drive.destroy unless drive.nil?
   redirect('/list')
 end
--- models/drive.rb
+++ models/drive.rb
@@ -1,23 +1,20 @@
 class Drive
   include DataMapper::Resource
 
-  property :id,                 Serial
-  property :bn,                 String
-  property :mn,                 String
+  property :bn,   String, :key => true
 
-  has 1, :drivemodel
+  has 1, :drive_model
 end
 
 class DriveModel
   include DataMapper::Resource
 
-  property :id,         Serial
-  property :mn,         String
-  property :brand,      Enum[:dell, :fujitsu, :seagate, :hitachi, :wd], :default => :seagate
-  property :size,       Integer
-  property :speed,      Integer
-  property :interface,  Enum[:scsi_u3, :scsi_u320, :scsi_u160, :pata, :sata, :other], :default => :scsi_Ultra3
+  property :model_number,   String, :key => true
+  property :brand,          Enum[:dell, :fujitsu, :seagate, :hitachi, :wd], :default => :seagate
+  property :size,           Integer
+  property :speed,          Integer
+  property :interface,      Enum[:scsi_u3, :scsi_u320, :scsi_u160, :pata, :sata, :other], :default => :scsi_Ultra3
 
-  belongs_to :Drive
+  belongs_to :drive
 
 end
